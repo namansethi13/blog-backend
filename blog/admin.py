1eefcacb2a597c8127bbf0a6807b7fd30ae920e8
stinger13 <93597591+stinger13@users.noreply.github.com>
2025-06-18 17:05:07 +0530

    some admin changes

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Post , Section
 
+
 admin.site.register(Post)
-admin.site.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    ordering = ('post__created_date','order')
+admin.site.register(Section, SectionAdmin)
